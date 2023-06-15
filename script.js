113d88d1d637af083ea1aaa556928be42d0fa30b
mxrgxr <mgxrcias@gmail.com>
2023-06-15 15:58:15 -0700

    update gameme psp pseudocode

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// Start with 4 tokens per slot (can be represented with numbers or token icons and reveal number when player hovers on slot)
-// Listen for player click on slot and start gameplay from there
-// Chosen slot is now empty, start gameplay by placing 1 token in each slot to the right of starting point
-    // Skip opponents store when moving counterclockwise around board
-    // Repeat if landing in slot with tokens regardless of side on board
-        // If no tokens && turn ends on player side && opponent slot directly across has tokens: collect opponent tokens and the one player just put down
-        // If no tokens && ((turn ends on player side && opponent has no tokens directly across) || turn ends on opponents side): forfeit turn
-    // If last move ends in own store: same player receives another turn else: switch player turn 
-// If no more tokens in 12 player slots: check for winner (highest count of tokens in store)&& enable reset button
-// Listen for player click on reset button and reset board
+// each player starts with 4 tokens/slot (24/player)
+// when player clicks slot
+    // 1. empty clicked slot
+    // 2. loop move 1 space to right drop 1 token as long as last move was in slot with more tokens than 1
+        // 2a. only place in own store, if passing over opponent store skip
+    // 3. possible last moves
+        // 3a. if player runs out of tokens after own store, player gains new turn
+        // 3b. if player runs out of tokens in opponent side, switch turns
+        // 3c. if player runs out of tokens in own side && opponent has tokens directly across, collect opponent tokens && last token player placed
+        // 3d. if player runs out of tokens in own side && opponent doesn't have tokens directly across, switch turns
+// if any tokens left in 12 playable slots, game is in progress
+    // 1. when no tokens in playable slots, check winner
+        // 1a. check winner function counts tokens in each player's stores
+        // 1b. player with > tokens wins
+        // 1c. enable reset button functionality
+// listen for player click on reset button to reset board and rerun the game
